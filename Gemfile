acc4301fee9c84e0c675f601c28b5fd6183901c4
nene <1231nnee@gmail.com>
2021-11-27 08:16:29 +0000

    rails05homework

--- Gemfile
+++ Gemfile
@@ -58,3 +58,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jquery-rails'
+
+gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  
+  get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+  
+  resources :users
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
